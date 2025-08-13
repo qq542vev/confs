40ba972757b8fd67819ce0ba07c408c8a05161c1
qq542vev <qq542vev@yahoo.co.jp>
2025-08-13 21:53:26 +0900

    chore: eslintの設定ファイルJSDOCの検証追加。

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,39 @@
+#!/usr/local/bin/eslint -c
+
 import js from "@eslint/js";
+import jsdoc from "eslint-plugin-jsdoc";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+	{
+		ignores: [
+			"**/node_modules/**",
+			"minified/**"
+		]
+	},
+	{
+		files: ["**/*.{js,mjs,cjs}"],
+		plugins: { js, jsdoc },
+		extends: [
+			js.configs.recommended,
+			jsdoc.configs["flat/recommended"]
+		],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			},
+		},
+		rules: {
+			"eqeqeq": ["error", "always"],
+			"init-declarations": ["warn", "always"],
+			"no-cond-assign": "warn",
+			"no-constant-condition": "error",
+			"no-else-return": "error",
+			"no-throw-literal": "error",
+			"prefer-const": "error",
+			"semi": ["error", "always"]
+		}
+	}
 ]);
