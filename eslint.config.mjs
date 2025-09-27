38cc81b2b69c75e7fad25ce890231ba7c54df11a
qq542vev <qq542vev@yahoo.co.jp>
2025-09-27 21:51:11 +0900

    feat: eslint.config.mjsにdublin-coreを追加。

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,8 +28,10 @@ import js from "@eslint/js";
 import jsdoc from "eslint-plugin-jsdoc";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import dublin from "eslint-config-dublin-core";
 
 export default defineConfig([
+	...dublin,
 	{
 		ignores: [
 			"**/node_modules/**",
