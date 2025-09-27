4f3c1d52593d90064c7e6e6946c7a53a1df856de
qq542vev <qq542vev@yahoo.co.jp>
2025-09-27 21:50:18 +0900

    docs: コメントを更新。

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,29 @@
 #!/usr/local/bin/eslint -c
 
+### File: eslint.config.mjs
+##
+## ESLint用の設定用ファイル。
+##
+## Usage:
+##
+## ------ Text ------
+## eslint -c eslint.config.mjs
+## ------------------
+##
+## Metadata:
+##
+##   id - f3cff8e5-c70a-444d-abaa-b47d25e60ccc
+##   author - <qq542vev at https://purl.org/meta/me/>
+##   version - 1.1.0
+##   created - 2025-07-21
+##   modified - 2025-09-27
+##   copyright - Copyright (C) 2025-2025 qq542vev. All rights reserved.
+##   license - <GNU GPLv3 at https://www.gnu.org/licenses/gpl-3.0.txt>
+##
+## See Also:
+##
+##   * <Project homepage at https://github.com/qq542vev/confs>
+
 import js from "@eslint/js";
 import jsdoc from "eslint-plugin-jsdoc";
 import globals from "globals";
